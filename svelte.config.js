8b203e877ecbf0b9187f5b26410d90063cbf34f0
Microphon3 <tanmay@heyfrontdesk.com>
2025-08-30 08:55:08 +0530

    fixed svelte-config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import ≈ from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: vitePreprocess(),
 	kit: {
+		adapter: adapter()
 		// SPA configuration for Cloudflare Pages
 		// adapter: adapter({
 		// 	fallback: 'index.html',
